d10f9570c2c894c35d69d887b007bfdfdc89bdb0
Anto231996 <tony2396@hotmail.it>
2022-03-09 17:25:06 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -31,4 +31,33 @@ const team = [
     },
 ];
 
-console.log(team);
+
+for (let key in team){
+    console.log (team [key]);
+    console.log (team [key].name);
+    console.log (team [key].role);
+    console.log (team [key].image);
+}
+
+let itemTeam = "";
+
+for (let key in team){
+    for (let i = 0; i < team.length; i++){
+        itemTeam += 
+        `
+        <div class="items col col-md-6 col-lg-4 mb-4 text-center">
+            <img src="${team[key].image}" alt="random picture">
+            <div class="description">
+                <h3>${team[key].name}</h3>
+                <h6>${team[key].role}</h6>
+            </div>
+        </div>;
+        `
+    }
+}
+
+const itemsElement = document.querySelector(".row");
+
+itemsElement.innerHTML = itemTeam;
+    
+console.log(team.length)
